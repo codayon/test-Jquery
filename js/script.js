5555b7576b881684007e30fc140cc2743ffe0687
codayon <reshadahammedayon@proton.me>
2025-02-26 01:46:16 +0600

    complete three type

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// ==================================
+
+$("#hide").click(function () {
+  $(".box").hide(400);
+});
+
+$("#show").click(function () {
+  $(".box").show(400);
+});
+
+$("#toggle").click(function () {
+  $(".box").toggle(400);
+});
+
+// ==================================
+
+$("#fadeOut").click(function () {
+  $(".box").fadeOut(400)
+})
+
+$("#fadeIn").click(function () {
+  $(".box").fadeIn(400)
+})
+
+$("#fadeToggle").click(function () {
+  $(".box").fadeToggle(400);
+});
+
+// ===================================
+
+$("#slideUp").click(function () {
+  $(".box").slideUp(400);
+});
+
+$("#slideDown").click(function () {
+  $(".box").slideDown(400);
+});
+
+$("#slideToggle").click(function () {
+  $(".box").slideToggle(400);
+});
+
