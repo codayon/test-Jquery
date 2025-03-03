c9c95aaafe4a573045c472d60179ac146c7918b4
codayon <reshadahammedayon@proton.me>
2025-03-03 17:55:26 +0600

    add venobox and mixitup section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// venobox section start
 new VenoBox({
   selector: ".my-image-links",
   numeration: true,
@@ -5,3 +6,20 @@ new VenoBox({
   share: true,
   spinner: "rotating-plane",
 });
+
+new VenoBox({
+  selector: '.my-video-links',
+});
+
+// venobox section end
+
+// mixitup section start
+var containerEl = document.querySelector('.mixitupSrc');
+
+var mixer = mixitup(containerEl, {
+  controls: {
+    toggleLogic: 'and'
+  }
+});
+
+// mixitup section start
